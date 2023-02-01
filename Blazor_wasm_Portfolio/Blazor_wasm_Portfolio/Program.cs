c72516fd3f2eb86c6aa338e69f34e89768aa10e4
tyeom <r.Until@gmail.com>
2023-02-01 17:03:26 +0900

    - Portfolio 사이트로 수정 후 커밋

---
+++ Blazor_wasm_Portfolio/Blazor_wasm_Portfolio/Program.cs
@@ -0,0 +1,13 @@
+using Blazor.AdminLte;
+using Blazor_wasm_Portfolio;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAdminLte();
+
+await builder.Build().RunAsync();
